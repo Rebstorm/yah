c2b919384627926f11743decd49107ed184772e7
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-02-14 19:50:22 +0100

    fixing the last lib

--- projects/yah-weather/src/lib/components/weather-button/weather-button.component.spec.ts
+++ projects/yah-weather/src/lib/components/weather-button/weather-button.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WeatherButtonComponent } from './weather-button.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 describe('WeatherButtonComponent', () => {
   let component: WeatherButtonComponent;
@@ -8,9 +11,9 @@ describe('WeatherButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WeatherButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [WeatherButtonComponent],
+      imports: [HttpClientTestingModule, NgxSpinnerModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +24,21 @@ describe('WeatherButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Information', () => {
+    it('should, contain weather information - if the sub is finished', () => {
+      const expectedNumber = -99999;
+      component.currentDegrees = expectedNumber;
+
+      fixture.detectChanges();
+
+      const introductionHeader = fixture.debugElement.query(
+        By.css('.next-hour')
+      );
+      expect(introductionHeader.nativeElement.innerText).toContain(
+        expectedNumber
+      );
+    });
   });
 });
--- projects/yah-weather/src/lib/services/weather.service.spec.ts
+++ projects/yah-weather/src/lib/services/weather.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WeatherService', () => {
   let service: WeatherService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(WeatherService);
   });
 
