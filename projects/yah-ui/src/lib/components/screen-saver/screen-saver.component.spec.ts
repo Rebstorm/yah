059e86b34839f6724c650dfd958e6ccd5c555be3
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-02-25 21:38:23 +0100

    more tests

--- projects/yah-ui/src/lib/components/screen-saver/screen-saver.component.spec.ts
+++ projects/yah-ui/src/lib/components/screen-saver/screen-saver.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScreenSaverComponent } from './screen-saver.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
 
 describe('ScreenSaverComponent', () => {
   let component: ScreenSaverComponent;
@@ -8,7 +10,8 @@ describe('ScreenSaverComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ScreenSaverComponent ]
+      declarations: [ ScreenSaverComponent ],
+      imports: [ RouterTestingModule ],
     })
     .compileComponents();
   });
@@ -22,4 +25,18 @@ describe('ScreenSaverComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Screensaver Functionality', () => {
+
+    it('it should, upon clicking the screen saver return to the main screen', () => {
+      const returnToMainScrenMethod = spyOn(component, 'returnToMainScren');
+      const divScreenSaver = fixture.debugElement.query(By.css('div'));
+
+      divScreenSaver.triggerEventHandler('click', {});
+
+      expect(returnToMainScrenMethod).toHaveBeenCalledTimes(1);
+
+    })
+
+  })
 });
