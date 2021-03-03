f174816ab69e79844084cb4edab8311572f75e52
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-03-03 18:59:58 +0100

    start

--- projects/yah-ui/src/lib/ui.module.ts
+++ projects/yah-ui/src/lib/ui.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScreenSaverComponent } from './components/screen-saver/screen-saver.component';
 import {RouterModule} from '@angular/router';
+import { AnalogueWatchComponent } from './components/analogue-watch/analogue-watch.component';
 
 
 
 @NgModule({
-  declarations: [ScreenSaverComponent],
+  declarations: [ScreenSaverComponent, AnalogueWatchComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
