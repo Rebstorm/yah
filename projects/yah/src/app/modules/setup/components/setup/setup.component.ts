bfcc791678b9a79caed984724e6655280cc8cc71
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-03-17 14:32:27 +0100

    continuation

--- projects/yah/src/app/modules/setup/components/setup/setup.component.ts
+++ projects/yah/src/app/modules/setup/components/setup/setup.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setup.component.scss'],
 })
 export class SetupComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
 
   goBack(): void {
     this.router.navigate(['main']).then();
+  }
+
+  //    outlet: 'options-outlet'
+  public routeToWeatherOption(): void {
+    this.router.navigate([ 'setup', { outlets: { options: ['weather']} } ], {} );
   }
 }
