5d2ac71a5c69f138193efb0689f5dedf80546411
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-03-29 12:43:16 +0200

    tests

--- projects/yah-ui/src/lib/components/counter/counter.component.spec.ts
+++ projects/yah-ui/src/lib/components/counter/counter.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
+import { By } from '@angular/platform-browser';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
@@ -8,9 +9,8 @@ describe('CounterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
-    })
-    .compileComponents();
+      declarations: [CounterComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,25 @@ describe('CounterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Counter', () => {
+    it('should increment the counter if pressing the plus', () => {
+      const incrementMethod = spyOn(component, 'increment');
+
+      const plusButton = fixture.debugElement.query(By.css('.plus'));
+      plusButton.triggerEventHandler('click', {});
+
+      expect(incrementMethod).toHaveBeenCalledTimes(1);
+    });
+
+    it('should decrease the counter if pressing the minus', () => {
+      const incrementMethod = spyOn(component, 'decrement');
+
+      const minusButton = fixture.debugElement.query(By.css('.min'));
+      minusButton.triggerEventHandler('click', {});
+
+      expect(incrementMethod).toHaveBeenCalledTimes(1);
+    });
   });
 });
