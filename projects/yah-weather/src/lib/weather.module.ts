45a5696ba0687fadac243849f6b465198fd414f4
Paul Jacks <Rebstorm@users.noreply.github.com>
2021-04-06 13:33:56 +0200

    fixing

--- projects/yah-weather/src/lib/weather.module.ts
+++ projects/yah-weather/src/lib/weather.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WeatherButtonComponent} from './components/weather-button/weather-button.component';
 import {NgxSpinnerModule} from 'ngx-spinner';
-import { WeatherSetupComponent } from './components/weather-setup/weather-setup.component';
+import {WeatherSetupComponent} from './components/weather-setup/weather-setup.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
-  declarations: [WeatherButtonComponent, WeatherSetupComponent],
+  declarations: [WeaXtherButtonComponent, WeatherSetupComponent],
   imports: [
     CommonModule,
+    NoopAnimationsModule,
     NgxSpinnerModule,
     ReactiveFormsModule
   ],
