25880bcabc78ff8d75542e26467902f95f3ae6b0
Paul Jacks <rebstorm@users.noreply.github.com>
2021-09-18 21:03:05 +0200

    added date parsing test

--- projects/yah-weather/src/lib/components/weather-details/weather-details.component.spec.ts
+++ projects/yah-weather/src/lib/components/weather-details/weather-details.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WeatherDetailsComponent } from './weather-details.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('WeatherDetailsComponent', () => {
   let component: WeatherDetailsComponent;
@@ -8,7 +10,9 @@ describe('WeatherDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WeatherDetailsComponent ]
+      declarations: [ WeatherDetailsComponent ],
+      imports: [HttpClientTestingModule],
+      providers: []
     })
     .compileComponents();
   });
@@ -22,4 +26,19 @@ describe('WeatherDetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Date parsing', () => {
+
+    it('should parse our date as expected time string', () => {
+
+      const date = new Date('01 Jan 1970 00:00:00 GMT');
+      const exptectedString = 'Fr. 1:00';
+
+      const result = component.humanReadableTime(date);
+
+      expect(result).toEqual(exptectedString);
+
+    });
+
+  })
 });
