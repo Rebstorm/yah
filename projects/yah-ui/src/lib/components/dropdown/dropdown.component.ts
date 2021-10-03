83fcb9ec5fb0e79bb9e6a2c16c24407b3632ad0a
Paul Jacks <rebstorm@users.noreply.github.com>
2021-10-03 17:38:51 +0200

    first initial idea

---
+++ projects/yah-ui/src/lib/components/dropdown/dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yah-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
